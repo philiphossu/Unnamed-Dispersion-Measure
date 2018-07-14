ab1e341753499ecbd25ffe75d4b2ba58c6f4cc1c
Philip Hossu <phossu@hawk.iit.edu>
2018-07-14 11:09:06 -0500

    Removed a bunch of unnecessary stuff in preparation for final week of research at Baylor

--- R/disp_est.R
+++ R/disp_est.R
@@ -2,9 +2,6 @@ disp_est <- function(pi_hat_emp) {
   one_vec_norm <- sqrt(length(pi_hat_emp))
   pi_hat_norm  <- sqrt(sum(pi_hat_emp^2))
   n            <- sum(pi_hat_emp)
-  print(n);
-  print(pi_hat_norm);
-  print(one_vec_norm);
   # Error is occuring Y on c(1,1,1), c(2,2,2), but N c(3,3,3)
   # Generally, error occurs when having to take acos(~1)
   if(is.na(ret <- acos(1 / (one_vec_norm * pi_hat_norm / n)))){
@@ -19,3 +16,5 @@ disp_est_cTest <- function(x) {
 }
 
 
+microbenchmark(disp_est(c(1,2,3,4)), times = 1000L)
+microbenchmark(disp_est_cTest(c(1,2,3,4)), times = 1000L)
